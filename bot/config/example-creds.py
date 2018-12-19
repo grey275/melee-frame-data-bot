97be80fe0da687156fc57891e102257fea8d335b
grey275 <pjvanderpol@gmail.com>
2018-12-19 14:51:03 -0800

    deleted old config dir

--- bot/config/example-creds.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-class Creds:
-    discord_token = ""
-
-    service_account_file = "./google-service-account.json"
-
-    service_account_creds = json.loads(open(service_account_file, "r").read())
